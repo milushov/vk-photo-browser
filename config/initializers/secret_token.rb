7d394da514afe506d36f503f9549653c8b195984
Nik Marchenko <nik.marchenko@gmail.com>
2013-07-04 16:03:56 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+VkPhotoBrowser::Application.config.secret_key_base = 'd0fa575b59a5f004eface1e632f5adcafbd9471a3cc200fcc8b1456a03d896ba61055458328102d484fd157d2b00905665169a0057a07a07e126bc7445b450df'
