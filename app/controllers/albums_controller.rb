53ab68af3ef2a4aeef459c5fc89f59f46a87cfde
Nik Marchenko <nik.marchenko@gmail.com>
2013-07-04 22:25:31 +0400

    Move caurrent_user method to application controller

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -6,13 +6,5 @@ class AlbumsController < ApplicationController
   def index
     @albums = @vk.photos.get_albums(uid: @user.uid, need_covers: 1).first
   end
-
-
-  private
-
-    def current_user
-      @vk   = VkontakteApi::Client.new(session[:token])
-      @user = @vk.users.get(uid: session[:vk_id], fields: [:screen_name, :photo]).first
-    end
-
+  
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  private
 
-  def user_should_be_authorized
-    redirect_to new_sessions_path unless session[:vk_id]
-  end
+    def user_should_be_authorized
+      redirect_to new_sessions_path unless session[:vk_id]
+    end
+
+    def current_user
+      @vk   = VkontakteApi::Client.new(session[:token])
+      @user = @vk.users.get(uid: session[:vk_id], fields: [:screen_name, :photo]).first
+    end
+
 end
